5ff1f8449f0723fa601f2ade940527461c4adfb8
Thiago Rocha <tpr@itsproject.cin.ufpe.br>
2017-09-25 16:22:30 -0300

    ClockIn Service

--- PontoFacil/PontoFacil/Services/ClockInService.cs
+++ PontoFacil/PontoFacil/Services/ClockInService.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PontoFacil.Models;
+using PontoFacil.Repositories;
+using System;
 
 namespace PontoFacil.Services
 {
     class ClockInService
     {
+        private IRepository<ClockIn> Rep;
+        private ClockIn clockIn;
+
+        public ClockInService(IRepository<ClockIn> rep)
+        {
+            Rep = rep;
+        }
+
+        public void Register(DateTime date)
+        {
+            if (clockIn.IsOpen)
+                EndCurrentDay(date);
+            else
+                StartNewDay(date);
+        }
+
+        private void StartNewDay(DateTime dt)
+        {
+            clockIn = new ClockIn();
+            clockIn.open(dt);
+        }
+
+        private void EndCurrentDay(DateTime dt)
+        {
+            clockIn.close(dt);
+            Rep.SaveClockIn(clockIn);
+        }
     }
 }
