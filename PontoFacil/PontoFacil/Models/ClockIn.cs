0e49a15fd04c8a477291fc42303194acca2cc338
Thiago Rocha <tpr@itsproject.cin.ufpe.br>
2017-09-25 16:22:30 -0300

    ClockIn Service

--- PontoFacil/PontoFacil/Models/ClockIn.cs
+++ PontoFacil/PontoFacil/Models/ClockIn.cs
@@ -8,5 +8,42 @@ namespace PontoFacil.Models
 {
     class ClockIn
     {
+        private bool _isOpen;
+        public bool IsOpen
+        {
+            get { return _isOpen; }
+            private set { _isOpen = value; }
+        }
+
+        private DateTime _start;
+        public DateTime Start
+        {
+            get { return _start; }
+            private set { _start = value; }
+        }
+
+        private DateTime _end;
+        public DateTime End
+        {
+            get { return _end; }
+            private set { _end = value; }
+        }
+
+        public void open(DateTime dt)
+        {
+            _start = dt;
+            _isOpen = false;
+        }
+
+        public void close(DateTime dt)
+        {
+            _end = dt;
+            _isOpen = false;
+        }
+
+        public ClockIn()
+        {
+            _isOpen = true;
+        }
     }
 }
