fe676e7d30dcdc50a77fbe63baaffc74d3d11355
Thiago Rocha <tpr@itsproject.cin.ufpe.br>
2017-09-25 14:18:44 -0300

    Planing Page

--- PontoFacil/PontoFacil/Views/PlanningPage.xaml.cs
+++ PontoFacil/PontoFacil/Views/PlanningPage.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+using Windows.System;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,9 +12,33 @@ namespace PontoFacil.Views
     /// </summary>
     public sealed partial class PlanningPage : Page
     {
+        private DateTime accumulatedHoursValue = DateTime.MinValue;
+        
+        private String hoursToAdjustValue = "";
+
+        private String NumberOfDaysValue = "";
+
+        private DateTime startDayValue = DateTime.Now;
+
         public PlanningPage()
         {
             this.InitializeComponent();
+
+            AccumulatedHoursValueXAML.Text = AccumulatedHoursValue.ToString();
+            
+            HoursToAdjustValueXAML.Text = hoursToAdjustValue.ToString();
+            
+            NumberOfDaysValueXAML.Text = NumberOfDaysValue.ToString();
+
+            StartDayValueXAML.Date = startDayValue;
+        }
+
+        public string AccumulatedHoursValue { get => accumulatedHoursValue.ToString("HH:mm"); set => accumulatedHoursValue = DateTime.MinValue; }
+        public string StartDayValue { get => startDayValue.ToString("HH:mm"); set => startDayValue = DateTime.MinValue; }
+
+        private void NumberTypeCheck(object sender, KeyRoutedEventArgs e)
+        {
+            e.Handled = ((uint)e.Key >= (uint)VirtualKey.Number0 && (uint)e.Key >= (uint)VirtualKey.Number9);
         }
     }
 }
