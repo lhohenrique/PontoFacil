d75d05600041cdd46c2af5ccc1e15bc3921ba704
lsmpf <lsmpf@ITS-LUCAS>
2017-09-25 09:37:20 -0300

    Add pages Settings, History and Planning

--- PontoFacil/PontoFacil/PageTokens.cs
+++ PontoFacil/PontoFacil/PageTokens.cs
@@ -3,5 +3,8 @@
     public static class PageTokens
     {
         public const string MainPage = "Main";
+        public const string Settings = "Settings";
+        public const string History = "History";
+        public const string Planning = "Planning";
     }
 }
